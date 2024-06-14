108b6d9e068ec73829390fa9159947c81e44c703
Fjerez7 <fjerezm07@gmail.com>
2024-06-13 19:19:17 -0500

    add routes for pages created

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,7 @@
 import {Navigate, useRoutes} from "react-router-dom";
 import {Today} from "../pages/Today/Today.tsx";
+import {Inbox} from "../pages/Inbox/Inbox.tsx";
+import {Upcoming} from "../pages/Upcoming/Upcoming.tsx";
 
 export const Root = () => {
     const routes = useRoutes([
@@ -10,6 +12,14 @@ export const Root = () => {
         {
             path: '/today',
             element: <Today />,
+        },
+        {
+            path: '/inbox',
+            element: <Inbox />
+        },
+        {
+            path: '/upcoming',
+            element: <Upcoming />
         }
     ]);
     return routes;
